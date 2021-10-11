2ecb14b2f9d1cbeb1a400e65e8081d0486107c2e
Mihir Momaya <mhmomaya@gmail.com>
2021-10-11 10:03:20 +0530

    Update my_dag.py

--- my_dag.py
+++ my_dag.py
@@ -24,15 +24,15 @@ def _training_model():
 
 with DAG("my_dag",start_date=datetime(2021,10,10),schedule_interval="@daily",catchup=False) as dag:
 
-    training_model_A=PythonOpertor(
+    training_model_A=PythonOperator(
     	task_id="training_model_A",
     	python_callable=_training_model)
     
-    training_model_B=PythonOpertor(
+    training_model_B=PythonOperator(
     	task_id="training_model_B",
     	python_callable=_training_model)
     
-    training_model_C=PythonOpertor(
+    training_model_C=PythonOperator(
     	task_id="training_model_C",
     	python_callable=_training_model)
 
